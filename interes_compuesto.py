ae9a7d2d130275c492cf269c0b416bf8dc82fb60
Kristian <krisjavermartinez@gmail.com>
2025-03-13 14:41:37 -0700

    Prueba a

---
+++ interes_compuesto.py
@@ -0,0 +1,19 @@
+# @Kristianmartinezcolina  -> Programación en Python
+"📌 Ejercicio de interés compuesto -> ¿Cómo calcular el valor futuro de una inversión?"
+
+""" Ejemplo con Gru de Mi Villano Favorito  
+Gru decide invertir $1,000 en una cuenta bancaria que ofrece un 3% de interés compuesto mensual.  
+Si deja su dinero en la cuenta durante 18 meses, ¿cuánto tendrá al final del período?  
+
+Pregunta:  
+¿Cuál será el valor futuro de la inversión de Gru después de 18 meses?  
+"""
+
+# --- Fórmula del Valor Futuro en Interés Compuesto ---  
+# VF = VP * (1 + r)  t  
+
+"""Datos  
+# VP -> Capital inicial = $1,000  
+# r -> Tasa de interés mensual en decimal (divide entre 100) = 3% = 0.03  
+# t -> Tiempo en meses = 18  
+"""  
